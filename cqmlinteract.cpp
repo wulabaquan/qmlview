39a6a6021e4ddb46ddcb1b5ba36fe20bfbb824b6
wulabaquan <wulabaquan@gmail.com>
2018-05-02 17:17:33 +0800

    dialog制作1,2

---
+++ cqmlinteract.cpp
@@ -0,0 +1,6 @@
+#include "cqmlinteract.h"
+
+CQMLinteract::CQMLinteract(QObject *parent) : QObject(parent)
+{
+
+}
---
+++ cqmlinteract.h
@@ -0,0 +1,17 @@
+#ifndef CQMLINTERACT_H
+#define CQMLINTERACT_H
+
+#include <QObject>
+
+class CQMLinteract : public QObject
+{
+    Q_OBJECT
+public:
+    explicit CQMLinteract(QObject *parent = nullptr);
+
+signals:
+
+public slots:
+};
+
+#endif // CQMLINTERACT_H
